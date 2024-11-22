daa00878b1eda85bcc82449f9a698058f97b4580
cornel-s-k <cornel.lydia@gmail.com>
2024-11-22 21:44:59 +0700

    Add files via upload

---
+++ views/pages/about/index.php
@@ -0,0 +1,42 @@
+<!-- ============================================================== -->
+<!-- Container fluid  -->
+<!-- ============================================================== -->
+<div class="container-fluid">
+
+    <?php $this->load->view('layouts/_alert') ?>
+
+    <div class="card-group">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/.profil.png') ?>">
+                    <div class="card-body">
+                        <h4 class="card-title">Idelia Larisa</h4>
+                        <p class="card-text">2310501087</p>  
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/profil.png') ?>">
+                    <div class="card-body">
+                        <h4 class="card-title">Mas Roro Putri Rhilowati Septiani</h4>
+                        <p class="card-text">2310501109</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/profil.png') ?>">
+                    <div class="card-body">
+                        <h4 class="card-title">Cornelia Lidya</h4>
+                        <p class="card-text">2310501107</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- End Container fluid  -->
+<!-- ============================================================== -->
